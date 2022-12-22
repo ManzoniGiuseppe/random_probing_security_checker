50431705a7f831aa01adecb8983d03c80342ac33
Giuseppe Manzoni <48492452+ManzoniGiuseppe@users.noreply.github.com>
2022-12-22 20:05:57 +0000

    Restructured addOp

--- src/addOp.h
+++ src/addOp.h
@@ -8,55 +8,160 @@
 #define _ADDOP_H_
 
 #include <stdint.h>
+#include <string.h>
+#include "addCore.h"
+#include "hashCache.h"
 #include "types.h"
 #include "hash.h"
 
 
 
-typedef struct { void *addOp; } addOp_t; // the storage of the add
-
-typedef uint64_t addOp_arr_t;   // an array of values
-typedef struct {
-  void *info;
-  T__THREAD_SAFE addOp_arr_t (*fast)(void *info, addOp_arr_t val);  // can be NULL
-  T__THREAD_SAFE void (*leaf)(void *info, hash_t val, bool isPos, hash_t *ret_val, bool *ret_isPos); // can be NULL if the other is != NULL
-} addOp_1Info_t;   // it describes an operation
-
-typedef struct {
-  void *info;
-  int same_op1;
-  int opposite_op1;
-
-  T__THREAD_SAFE bool (*isFastLeaf)(void *info, int paramNum, hash_t val, bool isNeg);
-  T__THREAD_SAFE addOp_arr_t (*fastLeaf)(void *info, int paramNum, hash_t val, bool isPos, addOp_arr_t other);
-
-  T__THREAD_SAFE void (*leaf)(void *info, hash_t val[2], bool isPos[2], hash_t *ret_val, bool *ret_isPos);
-} addOp_2Info_t;   // it describes an operation
+#define DBG_FILE  "addOp"
+#define DBG_LVL  DBG_ADDOP
 
 
+typedef struct{ addCore_arr_t param[2]; } addOp_cacheKey_t;
+typedef addCore_arr_t addOp_cacheVal_t;
 
 
-T__THREAD_SAFE addOp_t addOp_new(int numOp1, int numOp2, T__ACQUIRES addOp_1Info_t *op1s, T__ACQUIRES addOp_2Info_t *op2s);
-void addOp_delete(addOp_t storage);
+#define ADDOP__CACHE_ADD(cacheOp, p0, p1, RET, CODE) {\
+  addOp_cacheKey_t CACHE_ADD__key;\
+  memset(&CACHE_ADD__key, 0, sizeof(addOp_cacheKey_t));\
+  CACHE_ADD__key.param[0] = (p0);\
+  CACHE_ADD__key.param[1] = (p1);\
+\
+  if(hashCache_getValue(cacheOp, &CACHE_ADD__key, &RET)){\
+    ON_DBG(DBG_LVL_TOFIX, {\
+      if(RET == 0) FAIL("add: cacheOp's hashCache just returned a 0, which is an illegal index.\n")\
+    })\
+  }else{\
+    { CODE }\
+    hashCache_set(cacheOp, &CACHE_ADD__key, &RET);\
+    ON_DBG(DBG_LVL_TOFIX, {\
+      if(NUM_THREADS == 0){\
+        addOp_cacheVal_t CACHE_ADD__r;\
+        if(!hashCache_getValue(cacheOp, &CACHE_ADD__key, &CACHE_ADD__r)) FAIL("add: cacheOp's hashCache couldn't find a valud just added.")\
+        if(CACHE_ADD__r != RET) FAIL("add: cacheOp's hashCache returned a value that wasn't what inserted.")\
+      }\
+    })\
+  }\
+}
 
 
-T__THREAD_SAFE addOp_arr_t addOp_leaf(hash_t val, bool isPos);
-addOp_arr_t addOp_node(addOp_t storage, wire_t inputBit, addOp_arr_t val0, addOp_arr_t val1); // val0 and val1 must have a higher inputBit, or be a const.
-T__THREAD_SAFE addOp_arr_t addOp_neg(addOp_arr_t p);
 
-addOp_arr_t addOp_op1(addOp_t storage, int op, addOp_arr_t p);
-addOp_arr_t addOp_op2(addOp_t storage, int op, addOp_arr_t p0, addOp_arr_t p1);
+inline addCore_arr_t addOp_unary(
+  addCore_t s,
+  addCore_arr_t p,
+  void *info,
+  T__THREAD_SAFE void (*leaf)(void *info, hash_t val, bool isPos, hash_t *ret_val, bool *ret_isPos),
+  hashCache_t cache,
+  addCore_arr_t (*recursion)(void *info, addCore_t s, addCore_arr_t p)
+){
+  if(addCore_isLeafElseNode(p)){
+    hash_t val;
+    bool isPos;
+    addCore_getLeaf(p, &val, &isPos);
 
-T__THREAD_SAFE void addOp_flattenR(addOp_t s, addOp_arr_t p, wire_t maxDepth, hash_t *ret_hash, bool *ret_isPos);
+    hash_t ret_val;
+    bool ret_isPos;
+    leaf(info, val, isPos, &ret_val, &ret_isPos);
 
-T__THREAD_SAFE double addOp_dbg_storageFill(addOp_t storage);
-T__THREAD_SAFE double addOp_dbg_hashConflictRate(addOp_t storage);
+    return addCore_leaf(ret_val, ret_isPos);
+  }
+
+  addCore_arr_t ret;
+  ADDOP__CACHE_ADD(cache, p, p, ret, {
+    wire_t inputBit;
+    addCore_arr_t val0;
+    addCore_arr_t val1;
+    addCore_getNode(s, p, &inputBit, &val0, &val1);
+
+    val0 = recursion(info, s, val0);
+    val1 = recursion(info, s, val1);
+
+    ret = addCore_node(s, inputBit, val0, val1);
+  })
+  return ret;
+}
 
 
-// TODO: remove
-T__THREAD_SAFE bool addOp_isLeafElseNode(addOp_arr_t p);
-T__THREAD_SAFE void addOp_getLeaf(addOp_arr_t p, hash_t *ret_val, bool *ret_isPos); // if a ret_ is null, it's not set
-T__THREAD_SAFE void addOp_getNode(addOp_t storage, addOp_arr_t p, wire_t *ret_inputBit, addOp_arr_t *ret_val0, addOp_arr_t *ret_val1); // if a ret_ is null, it's not set
+inline addCore_arr_t addOp_binary(
+  addCore_t s,
+  addCore_arr_t p0,
+  addCore_arr_t p1,
+  void *info,
+  T__THREAD_SAFE addCore_arr_t (*same_op)(void *info, addCore_arr_t val),
+  T__THREAD_SAFE addCore_arr_t (*opposite_op)(void *info, addCore_arr_t val),
+  T__THREAD_SAFE bool (*isFastLeaf)(void *info, int paramNum, hash_t val, bool isNeg),
+  T__THREAD_SAFE addCore_arr_t (*fastLeaf)(void *info, int paramNum, hash_t val, bool isPos, addCore_arr_t other),
+  T__THREAD_SAFE void (*fullLeaf)(void *info, hash_t val[2], bool isPos[2], hash_t *ret_val, bool *ret_isPos),
+  hashCache_t cache,
+  addCore_arr_t (*recursion)(void *info, addCore_t s, addCore_arr_t p0, addCore_arr_t p1)
+){
+  if(p0 == p1) return same_op(info, p0);
+  if(p0 == addCore_neg(p1)) return opposite_op(info, p0);
+
+  addCore_arr_t p[2] = {p0, p1};
+
+  bool c[2];
+  for(int k = 0; k < 2; k++)
+    c[k] = addCore_isLeafElseNode(p[k]);
+
+  if(c[0] && c[1]){
+    hash_t val[2];
+    bool isPos[2];
+    for(int k = 0; k < 2; k++)
+      addCore_getLeaf(p[k], &val[k], &isPos[k]);
+
+    hash_t ret_val;
+    bool ret_isPos;
+    fullLeaf(info, val, isPos, &ret_val, &ret_isPos);
+
+    return addCore_leaf(ret_val, ret_isPos);
+  }
+
+  for(int k = 0; k < 2; k++)
+    if(c[k]){
+      hash_t val;
+      bool isPos;
+      addCore_getLeaf(p[k], &val, &isPos);
+
+      if(isFastLeaf(info, k, val, isPos))
+        return fastLeaf(info, k, val, isPos, p[1-k]);
+    }
+
+
+  addCore_arr_t ret;
+  ADDOP__CACHE_ADD(cache, p0, p1, ret, {
+    addCore_arr_t a[2][2];
+    wire_t w[2];
+
+    for(int k = 0; k < 2; k++)
+      addCore_getNode(s, p[k], &w[k], &a[k][0], &a[k][1]);
+
+    int min = w[0] <= w[1] ? 0 : 1;  // parameter with the minimal input, minimal -> first.
+    int max = 1-min; // parameter with the other input.
+
+    addCore_arr_t sub[2];
+    if(w[min] == w[max]){
+      for(int l = 0; l < 2; l++)
+        sub[l] = recursion(info, s, a[0][l], a[1][l]);
+    }else if(min == 0){
+      for(int l = 0; l < 2; l++)
+        sub[l] = recursion(info, s, a[0][l], p[1]);   // only open the one with the first input, leave the other unchanged.
+    }else{
+      for(int l = 0; l < 2; l++)
+        sub[l] = recursion(info, s, p[0], a[1][l]);   // only open the one with the first input, leave the other unchanged.
+    }
+
+    ret = addCore_node(s, w[min], sub[0], sub[1]);
+  })
+  return ret;
+}
+
+
+#undef DBG_FILE
+#undef DBG_LVL
 
 
 
--- src/bdd.c
+++ src/bdd.c
@@ -11,41 +11,47 @@
 #include "string.h"
 
 
-#define OP1_ID  0
-#define OP1_0   1
-#define OP1_1   2
-#define OP2_AND  0
-#define OP2_XOR  1
 
+#define BDD_CONST(x)  addCore_leaf((hash_t){0}, (x))
 
-#define BDD_CONST(x)  addOp_leaf((hash_t){0}, (x))
+T__THREAD_SAFE static inline addCore_arr_t op1_id(__attribute__((unused)) void *info, addCore_arr_t val){ return val; }
+T__THREAD_SAFE static inline addCore_arr_t op1_0(__attribute__((unused)) void *info, __attribute__((unused)) addCore_arr_t val){ return BDD_CONST(0); }
+T__THREAD_SAFE static inline addCore_arr_t op1_1(__attribute__((unused)) void *info, __attribute__((unused)) addCore_arr_t val){ return BDD_CONST(1); }
 
-T__THREAD_SAFE static addOp_arr_t op1_id(__attribute__((unused)) void *info, addOp_arr_t val){ return val; }
-T__THREAD_SAFE static addOp_arr_t op1_0(__attribute__((unused)) void *info, __attribute__((unused)) addOp_arr_t val){ return BDD_CONST(0); }
-T__THREAD_SAFE static addOp_arr_t op1_1(__attribute__((unused)) void *info, __attribute__((unused)) addOp_arr_t val){ return BDD_CONST(1); }
-T__THREAD_SAFE bool always_isFastLeaf(__attribute__((unused)) void *info, __attribute__((unused)) int paramNum, __attribute__((unused)) hash_t val, __attribute__((unused)) bool isPos){ return 1; }
-T__THREAD_SAFE void op2_and_leaf(__attribute__((unused)) void *info, __attribute__((unused)) hash_t val[2], bool isPos[2], hash_t *ret_val, bool *ret_isPos){
+T__THREAD_SAFE static inline bool always_isFastLeaf(__attribute__((unused)) void *info, __attribute__((unused)) int paramNum, __attribute__((unused)) hash_t val, __attribute__((unused)) bool isPos){ return 1; }
+T__THREAD_SAFE static inline void op2_and_fullLeaf(__attribute__((unused)) void *info, __attribute__((unused)) hash_t val[2], bool isPos[2], hash_t *ret_val, bool *ret_isPos){
   *ret_val = (hash_t){0};
   *ret_isPos = isPos[0] && isPos[1];
 }
-T__THREAD_SAFE addOp_arr_t op2_and_fastLeaf(__attribute__((unused)) void *info, __attribute__((unused)) int paramNum, __attribute__((unused)) hash_t val, bool isPos, addOp_arr_t other){
+T__THREAD_SAFE static inline addCore_arr_t op2_and_fastLeaf(__attribute__((unused)) void *info, __attribute__((unused)) int paramNum, __attribute__((unused)) hash_t val, bool isPos, addCore_arr_t other){
   if(isPos) return other;
   else return BDD_CONST(0);
 }
-T__THREAD_SAFE void op2_xor_leaf(__attribute__((unused)) void *info, __attribute__((unused)) hash_t val[2], bool isPos[2], hash_t *ret_val, bool *ret_isPos){
+T__THREAD_SAFE static inline void op2_xor_fullLeaf(__attribute__((unused)) void *info, __attribute__((unused)) hash_t val[2], bool isPos[2], hash_t *ret_val, bool *ret_isPos){
   *ret_val = (hash_t){0};
   *ret_isPos = isPos[0] ^ isPos[1];
 }
-T__THREAD_SAFE addOp_arr_t op2_xor_fastLeaf(__attribute__((unused)) void *info, __attribute__((unused)) int paramNum, __attribute__((unused)) hash_t val, bool isPos, addOp_arr_t other){
-  if(isPos) return addOp_neg(other);
+T__THREAD_SAFE static inline addCore_arr_t op2_xor_fastLeaf(__attribute__((unused)) void *info, __attribute__((unused)) int paramNum, __attribute__((unused)) hash_t val, bool isPos, addCore_arr_t other){
+  if(isPos) return addCore_neg(other);
   else return other;
 }
 
+static addCore_arr_t fn_and(void *info, addCore_t s, addCore_arr_t p0, addCore_arr_t p1){
+  hashCache_t *cache = info;
+  return addOp_binary(s, p0, p1, info, op1_id, op1_0, always_isFastLeaf, op2_and_fastLeaf, op2_and_fullLeaf, *cache, fn_and);
+}
+
+static addCore_arr_t fn_xor(void *info, addCore_t s, addCore_arr_t p0, addCore_arr_t p1){
+  hashCache_t *cache = info;
+  return addOp_binary(s, p0, p1, info, op1_0, op1_1, always_isFastLeaf, op2_xor_fastLeaf, op2_xor_fullLeaf, *cache, fn_xor);
+}
+
+
 
 typedef struct {
-  addOp_t a;
-  addOp_1Info_t *op1s;
-  addOp_2Info_t *op2s;
+  addCore_t a;
+  hashCache_t cache_and;
+  hashCache_t cache_xor;
 } bdd_storage_t;
 #define P(pub)   ((bdd_storage_t *) ((bdd_t) (pub)).bdd)
 
@@ -55,56 +61,31 @@ typedef struct {
 bdd_t bdd_storage_alloc(void){
   bdd_t ret = { mem_calloc(sizeof(bdd_storage_t), 1, "BDD main struct") };
 
-  P(ret)->op1s = mem_calloc(sizeof(addOp_1Info_t), 3, "BDD array of op1");
-  P(ret)->op1s[OP1_ID].info = NULL;
-  P(ret)->op1s[OP1_ID].fast = op1_id;
-  P(ret)->op1s[OP1_ID].leaf = NULL;
+  P(ret)->a = addCore_new();
+  P(ret)->cache_and = hashCache_new(sizeof(addOp_cacheKey_t), sizeof(addOp_cacheVal_t), "BDD, a cache for and");
+  P(ret)->cache_xor = hashCache_new(sizeof(addOp_cacheKey_t), sizeof(addOp_cacheVal_t), "BDD, a cache for xor");
 
-  P(ret)->op1s[OP1_0].info = NULL;
-  P(ret)->op1s[OP1_0].fast = op1_0;
-  P(ret)->op1s[OP1_0].leaf = NULL;
-
-  P(ret)->op1s[OP1_1].info = NULL;
-  P(ret)->op1s[OP1_1].fast = op1_1;
-  P(ret)->op1s[OP1_1].leaf = NULL;
-
-  P(ret)->op2s = mem_calloc(sizeof(addOp_2Info_t), 2, "BDD array of op1");
-  P(ret)->op2s[OP2_AND].info = NULL;
-  P(ret)->op2s[OP2_AND].same_op1 = OP1_ID;
-  P(ret)->op2s[OP2_AND].opposite_op1 = OP1_0;
-  P(ret)->op2s[OP2_AND].isFastLeaf = always_isFastLeaf;
-  P(ret)->op2s[OP2_AND].fastLeaf = op2_and_fastLeaf;
-  P(ret)->op2s[OP2_AND].leaf = op2_and_leaf;
-
-  P(ret)->op2s[OP2_XOR].info = NULL;
-  P(ret)->op2s[OP2_XOR].same_op1 = OP1_0;
-  P(ret)->op2s[OP2_XOR].opposite_op1 = OP1_1;
-  P(ret)->op2s[OP2_XOR].isFastLeaf = always_isFastLeaf;
-  P(ret)->op2s[OP2_XOR].fastLeaf = op2_xor_fastLeaf;
-  P(ret)->op2s[OP2_XOR].leaf = op2_xor_leaf;
-
-  P(ret)->a = addOp_new(3, 2, P(ret)->op1s, P(ret)->op2s);
   return ret;
 }
 void bdd_storage_free(bdd_t s){
-  addOp_delete(B2A(s));
-  mem_free(P(s)->op1s);
-  mem_free(P(s)->op2s);
+  addCore_delete(B2A(s));
+  hashCache_delete(P(s)->cache_and);
+  hashCache_delete(P(s)->cache_xor);
   mem_free(P(s));
 }
 
 bdd_fn_t bdd_val_const(__attribute__((unused)) bdd_t s, bool val){ return BDD_CONST(val); }
-bdd_fn_t bdd_val_single(bdd_t s, wire_t inputBit){ return addOp_node(B2A(s), inputBit, BDD_CONST(0), BDD_CONST(1)); }
+bdd_fn_t bdd_val_single(bdd_t s, wire_t inputBit){ return addCore_node(B2A(s), inputBit, BDD_CONST(0), BDD_CONST(1)); }
 
-bdd_fn_t bdd_op_not(__attribute__((unused)) bdd_t s, bdd_fn_t val){ return addOp_neg(val); }
-bdd_fn_t bdd_op_and(bdd_t s, bdd_fn_t val0, bdd_fn_t val1){ return addOp_op2(B2A(s), OP2_AND, val0, val1); }
-bdd_fn_t bdd_op_or(bdd_t s, bdd_fn_t val0, bdd_fn_t val1){ return addOp_neg(bdd_op_and(s, addOp_neg(val0), addOp_neg(val1))); }
-bdd_fn_t bdd_op_xor(bdd_t s, bdd_fn_t val0, bdd_fn_t val1){ return addOp_op2(B2A(s), OP2_XOR, val0, val1); }
+bdd_fn_t bdd_op_not(__attribute__((unused)) bdd_t s, bdd_fn_t val){ return addCore_neg(val); }
+bdd_fn_t bdd_op_and(bdd_t s, bdd_fn_t val0, bdd_fn_t val1){ return fn_and(&P(s)->cache_and, B2A(s), val0, val1); }
+bdd_fn_t bdd_op_xor(bdd_t s, bdd_fn_t val0, bdd_fn_t val1){ return fn_xor(&P(s)->cache_xor, B2A(s), val0, val1); }
+bdd_fn_t bdd_op_or(bdd_t s, bdd_fn_t val0, bdd_fn_t val1){ return addCore_neg(bdd_op_and(s, addCore_neg(val0), addCore_neg(val1))); }
 
-T__THREAD_SAFE void bdd_flattenR(bdd_t s, bdd_fn_t p, wire_t maxDepth, hash_t *ret_hash, bool *ret_isPos){ addOp_flattenR(B2A(s), p, maxDepth, ret_hash, ret_isPos); }
+T__THREAD_SAFE void bdd_flattenR(bdd_t s, bdd_fn_t p, wire_t maxDepth, hash_t *ret_hash, bool *ret_isPos){ addCore_flattenR(B2A(s), p, maxDepth, ret_hash, ret_isPos); }
 
-T__THREAD_SAFE double bdd_dbg_storageFill(bdd_t s){ return addOp_dbg_storageFill(B2A(s)); }
-T__THREAD_SAFE double bdd_dbg_hashConflictRate(bdd_t s){ return addOp_dbg_hashConflictRate(B2A(s)); }
+T__THREAD_SAFE double bdd_dbg_storageFill(bdd_t s){ return addCore_dbg_storageFill(B2A(s)); }
+T__THREAD_SAFE double bdd_dbg_hashConflictRate(bdd_t s){ return addCore_dbg_hashConflictRate(B2A(s)); }
 
 
 
@@ -165,10 +146,10 @@ void bdd_sumCached_delete(bdd_sumCached_t cached){
 T__THREAD_SAFE static void sumRnd(bdd_sumCached_t c, bdd_fn_t val, number_t *ret){
   size_t size = PC(c)->numberSize;
 
-  if(addOp_isLeafElseNode(val)){
+  if(addCore_isLeafElseNode(val)){
     number_one(size, PC(c)->numRnds, ret);
     bool isPos;
-    addOp_getLeaf(val, NULL, &isPos);
+    addCore_getLeaf(val, NULL, &isPos);
 
     if(isPos)   // (-1)^F = (-1)^0 = 1        (-1)^T = (-1)^1 = -1
       number_local_negation(size, ret);
@@ -177,7 +158,7 @@ T__THREAD_SAFE static void sumRnd(bdd_sumCached_t c, bdd_fn_t val, number_t *ret
 
   CACHE_SUM_ADD(c, val, *ret, {
     bdd_fn_t v[2];
-    addOp_getNode(B2A(PC(c)->storage), val, NULL, &v[0], &v[1]);
+    addCore_getNode(B2A(PC(c)->storage), val, NULL, &v[0], &v[1]);
 
     number_t r0[size];
     sumRnd(c, v[0], r0);
@@ -214,7 +195,7 @@ T__THREAD_SAFE static void doSubTransform(bdd_sumCached_t c, bdd_fn_t val, numbe
   size_t numMaskedIns = PC(c)->numMaskedIns;
   size_t numSize = PC(c)->numberSize;
 
-  if(addOp_isLeafElseNode(val)){
+  if(addCore_isLeafElseNode(val)){
     sumRnd(c, val, & ret[lowest * numSize]);
     doSubTransform_block(c, ret, lowest, input, numMaskedIns - input);
     return;
@@ -222,7 +203,7 @@ T__THREAD_SAFE static void doSubTransform(bdd_sumCached_t c, bdd_fn_t val, numbe
 
   wire_t in;
   bdd_fn_t a[2];
-  addOp_getNode(B2A(PC(c)->storage), val, &in, &a[0], &a[1]);
+  addCore_getNode(B2A(PC(c)->storage), val, &in, &a[0], &a[1]);
 
   if(in >= numMaskedIns){
     sumRnd(c, val, & ret[lowest * numSize]);
